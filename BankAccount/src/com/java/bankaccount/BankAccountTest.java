763152b86cfa527b81401645f376f14e40d45ff5
devongabrielpena@gmail.com <devongabriel@DevonGabriels-MacBook-Pro.local>
2019-08-09 12:29:33 -0700

    added complete bank account assignment

--- BankAccount/src/com/java/bankaccount/BankAccountTest.java
+++ BankAccount/src/com/java/bankaccount/BankAccountTest.java
@@ -1,9 +1,23 @@
 package com.java.bankaccount;
 
 public class BankAccountTest {
-	public static void main(String[] args) {
-		BankAccount b = new BankAccount();
-		
-		System.out.println(b.getAccountNumber());
-	}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			BankAccount b = new BankAccount();
+			BankAccount b1 = new BankAccount();
+			
+			System.out.println(b.getAccountNumber());
+			System.out.println(b1.getAccountNumber());
+			
+			b.deposit("checking", 1000.0);
+			b.deposit("savings", 100.0);
+			
+			
+			System.out.println(b.getChecking());
+			System.out.println(b.getSavings());
+			System.out.println(BankAccount.getTotal());
+			System.out.println(BankAccount.getNumberOfAccounts());
+			
+
+		}
 }

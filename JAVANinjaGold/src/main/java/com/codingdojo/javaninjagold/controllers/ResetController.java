1356a8c00a4bb985b1aa7f1a2623e59958de2228
devongabrielpena@gmail.com <devongabriel@DevonGabriels-MacBook-Pro.local>
2019-08-11 20:44:45 -0700

    JAVA Spring Update and Delete Assignment

--- JAVANinjaGold/src/main/java/com/codingdojo/javaninjagold/controllers/ResetController.java
+++ JAVANinjaGold/src/main/java/com/codingdojo/javaninjagold/controllers/ResetController.java
@@ -1,5 +1,15 @@
 package com.codingdojo.javaninjagold.controllers;
 
-public class ResetController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResetController {
+	@RequestMapping("/reset")
+	public String reset(HttpSession session) {
+	session.removeAttribute("gold");
+	return "redirect:/";
+	}
 }
